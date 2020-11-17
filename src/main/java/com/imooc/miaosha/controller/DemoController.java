94932209b6a694c5a934beabda18dc0a94f70e84
Qunling Wang <qwang@ddswireless.com>
2020-11-17 11:20:24 -0800

    added redis

--- src/main/java/com/imooc/miaosha/controller/DemoController.java
+++ src/main/java/com/imooc/miaosha/controller/DemoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.imooc.miaosha.domain.User;
 import com.imooc.miaosha.redis.RedisService;
+import com.imooc.miaosha.redis.UserKey;
 import com.imooc.miaosha.result.CodeMsg;
 import com.imooc.miaosha.result.Result;
 import com.imooc.miaosha.service.UserService;
@@ -69,9 +70,20 @@ public class DemoController {
 	
 	@RequestMapping("/redis/get")
 	@ResponseBody
-	public Result<Long> redisGet() {
-		Long v1 = redisService.get("key1", Long.class);
+	public Result<User> redisGet() {
+		User v1 = redisService.get(UserKey.getById, "" + 1, User.class);
 		return Result.success(v1);
+	}
+	
+	@RequestMapping("/redis/set")
+	@ResponseBody
+	public Result<Boolean> redisSet() {
+		User user = new User();
+		user.setId(1);
+		user.setName("1111");
+		boolean ret = redisService.set(UserKey.getById, "" + 1, user);
+//		String str = redisService.get("key2", String.class);
+		return Result.success(ret);
 	}
 
 }
